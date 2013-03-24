7f86c5dccdbd441543159224a67b3f3714856a9e
Bill Couch <couch@twitter.com>
2013-03-23 21:38:17 -0700

    Resolved merge conflict, fixed js nav animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 function hideHeader() {
-  document.querySelector('header').classList.remove('force-hovering');  
+  document.querySelector('header').classList.remove('fixed-hover');
 }
 
 function main() {
   document.querySelector('header').classList.add('floating');
-  document.querySelector('header').classList.add('force-hovering');
+  document.querySelector('header').classList.add('fixed-hover');
 
-  window.setTimeout(hideHeader, 2500);
+  if (!document.body.classList.contains('homepage')){
+    window.setTimeout(hideHeader, 2500);
+  }
 }
